f5fe8bb4595c2af01bc31654469b5bcf87fb110b
Maksim Krasavtsev <777maxim059@gmail.com>
2026-06-01 19:46:39 +0700

    3.0review

--- core/constants.py
+++ core/constants.py
@@ -9,12 +9,14 @@ MAX_PHONE_LEN = 12
 
 MAX_STATUS_LEN = 6
 
-DEFAULT_SIZE = (200, 200)
-DEFAULT_FONT_SIZE = 100
-DEFAULT_TEXT_COLOR = (255, 255, 255)
+AVATAR_SIZE = (200, 200)
+AVATAR_FONT_SIZE = 140
+AVATAR_TEXT_COLOR = (255, 255, 255)
+AVATAR_VERTICAL_OFFSET = -30
 BBOX_COORDINATES = (0, 0)
 
 FONT_PATHS = [
+    "static/fonts/Neue_Haas_Grotesk_Display_Pro_75_Bold.otf",
     "arial.ttf",
     "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
     "/System/Library/Fonts/Helvetica.ttc",
--- users/utils.py
+++ users/utils.py
@@ -9,6 +9,7 @@ from core.constants import (
     AVATAR_FONT_SIZE,
     AVATAR_SIZE,
     AVATAR_TEXT_COLOR,
+    AVATAR_VERTICAL_OFFSET,
     BBOX_COORDINATES,
     FONT_PATHS,
 )
@@ -38,7 +39,7 @@ def generate_avatar(name, email, size=None, text_color=None):
     bbox = draw.textbbox(BBOX_COORDINATES, letter, font=font)
     text_width = bbox[2] - bbox[0]
     text_height = bbox[3] - bbox[1]
-    position = ((size[0] - text_width) // 2, (size[1] - text_height) // 2)
+    position = ((size[0] - text_width) // 2, (size[1] - text_height) // 2  + AVATAR_VERTICAL_OFFSET)
     draw.text(position, letter, fill=text_color, font=font)
 
     buffer = BytesIO()
